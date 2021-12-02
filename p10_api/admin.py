a9e30003fb12f61cbefa8f43299b35742ccd3cf2
ashutosh raghuwanshi <58286940+ulpa11@users.noreply.github.com>
2021-12-02 15:03:09 +0530

    Add files via upload

---
+++ p10_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import P10Status
+# Register your models here.
+admin.site.register(P10Status)
---
+++ p10_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class P10ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'p10_api'
